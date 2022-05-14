a071867ff5661792f25946426f54eb87b646b09c
Tonio <tonio.castro@gmail.com>
2022-05-14 18:30:00 +0200

    💥 refactor: Removed ImageCapture dependency.

--- src/containers/TakePicture/index.tsx
+++ src/containers/TakePicture/index.tsx
@@ -38,10 +38,6 @@ export const TakePicture = () => {
 
   const mediaStream = useCamera(CAPTURE_OPTIONS);
 
-  if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
-    videoRef.current.srcObject = mediaStream;
-  }
-
   function handleCanPlay() {
     videoRef.current?.play();
   }
@@ -58,62 +54,57 @@ export const TakePicture = () => {
   }, []);
 
   useEffect(() => {
-    const displayAndCheckImage = async (capture: ImageCapture) => {
-      const photo = await capture.takePhoto();
-      const url = URL.createObjectURL(photo);
-      const img = new Image();
-      img.src = url;
+    if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
+      videoRef.current.srcObject = mediaStream;
+    }
+  }, [mediaStream, videoRef.current]);
 
-      img.addEventListener("load", () => {
-        if (canvasRef.current) {
-          const cutStartX = ~~img.width * 0.1;
-          const cutWidth = ~~img.width * 0.8;
-          const cutHeight = ~~cutWidth * ID_HEIGHT_RATIO;
-          const cutStartY = ~~img.height / 2 - cutHeight / 2;
-          URL.revokeObjectURL(img.src);
-          const context = canvasRef.current.getContext("2d");
-          context?.drawImage(
-            img,
-            cutStartX,
-            cutStartY,
-            cutWidth,
-            cutHeight,
-            0,
-            0,
-            ID_CAPTURE_WIDTH,
-            ID_CAPTURE_HEIGHT
-          );
-          const data = context?.getImageData(
-            0,
-            0,
-            ID_CAPTURE_WIDTH,
-            ID_CAPTURE_HEIGHT
-          );
-          const [r, g, b] = getAverageRGBfromImage(data);
-          if (isValidIdCardAverageColor(r, g, b)) {
-            const imageData = canvasRef.current.toDataURL();
-            setBlobPhoto(imageData);
-            handleSubmit(imageData);
-            setColorIsValidated(true);
-            clearInterval(timmer);
-            processEnd = true;
-          }
-        }
-      });
-    };
-
-    const captureAndProcessImage = async () => {
-      if (videoRef.current?.srcObject && !processEnd) {
-        const tracks = mediaStream?.getVideoTracks();
-        if (tracks) {
-          const capture = new ImageCapture(tracks[0]);
-          await displayAndCheckImage(capture);
+  useEffect(() => {
+    const displayAndCheckImage = async () => {
+      if (canvasRef.current && videoRef.current) {
+        await videoRef.current.play();
+        const context = canvasRef.current.getContext("2d");
+        const cutStartX = ~~videoRef.current.videoWidth * 0.1;
+        const cutWidth = ~~videoRef.current.videoWidth * 0.8;
+        const cutHeight = ~~cutWidth * ID_HEIGHT_RATIO;
+        const cutStartY = ~~videoRef.current.videoHeight / 2 - cutHeight / 2;
+        context?.drawImage(
+          videoRef.current,
+          cutStartX,
+          cutStartY,
+          cutWidth,
+          cutHeight,
+          0,
+          0,
+          ID_CAPTURE_WIDTH,
+          ID_CAPTURE_HEIGHT
+        );
+        const data = context?.getImageData(
+          0,
+          0,
+          ID_CAPTURE_WIDTH,
+          ID_CAPTURE_HEIGHT
+        );
+        const [r, g, b] = getAverageRGBfromImage(data);
+        if (isValidIdCardAverageColor(r, g, b)) {
+          const imageData = canvasRef.current.toDataURL();
+          setBlobPhoto(imageData);
+          handleSubmit(imageData);
+          setColorIsValidated(true);
+          clearInterval(timmer);
+          processEnd = true;
         }
       }
     };
 
+    const captureAndProcessImage = async () => {
+      if (videoRef.current?.srcObject && !processEnd) {
+        await displayAndCheckImage();
+      }
+    };
+
     captureAndProcessImage();
-    const timmer = setInterval(captureAndProcessImage, 500);
+    const timmer = setInterval(captureAndProcessImage, 300);
 
     return () => {
       clearInterval(timmer);
