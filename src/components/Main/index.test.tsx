672ea4d7a7f15d71c1ccbd1d4511884495f592b2
Tonio <tonio.castro@gmail.com>
2022-05-14 14:17:47 +0200

    ✅ test: corrected typo in Main test

--- src/components/Main/index.test.tsx
+++ src/components/Main/index.test.tsx
@@ -3,7 +3,7 @@ import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { Main } from "./index";
 
-describe("Header", () => {
+describe("Main", () => {
   it("renders texts", () => {
     render(
       <Main
